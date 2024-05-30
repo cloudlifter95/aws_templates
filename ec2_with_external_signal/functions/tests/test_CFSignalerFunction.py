106de1c19c9bba642b5a0c6e4812717132922693
Mohammed Jennane (myjen) <mo.y.jennane@gmail.com>
2024-05-30 22:55:50 +0200

    last assertion first test

--- ec2_with_external_signal/functions/tests/test_CFSignalerFunction.py
+++ ec2_with_external_signal/functions/tests/test_CFSignalerFunction.py
@@ -3,6 +3,7 @@ import pytest
 import boto3
 from moto import mock_aws
 from unittest.mock import patch, MagicMock
+from io import StringIO
 from datetime import datetime, timezone
 import os
 import logging
@@ -23,7 +24,8 @@ from CFSignalerFunction.app import enrich_event_with_ec2_resource_id
 
 
 @patch('CFSignalerFunction.app.EC2_CLIENT.describe_instances')
-def test_enrich_event_with_ec2_resource_id(mock_describe_instances, setup_environment_and_env_vars, event_from_scheduler):
+@patch('sys.stdout', new_callable=StringIO)
+def test_enrich_event_with_ec2_resource_id(mock_stdout, mock_describe_instances, setup_environment_and_env_vars, event_from_scheduler):
     mock_response = {
         'Reservations': [
             {
@@ -38,3 +40,16 @@ def test_enrich_event_with_ec2_resource_id(mock_describe_instances, setup_enviro
 
     result_event = enrich_event_with_ec2_resource_id(event_from_scheduler)
     assert result_event['ResourceProperties']['ec2_resource_id'] == 'instance_id2'
+
+    # Second assertion
+    mock_response = {
+        'Reservations': []
+    }
+    mock_describe_instances.return_value = mock_response
+    result_event = enrich_event_with_ec2_resource_id(event_from_scheduler)
+    # Capture the printed output
+    printed_output = mock_stdout.getvalue().strip()
+    logging.info(f"printed_output:{printed_output}")
+    # Assertions
+    assert printed_output == "No instances found with the specified tag."
+    assert 'ec2_resource_id' not in result_event
